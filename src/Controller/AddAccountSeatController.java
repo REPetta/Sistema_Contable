89ea7d96df0d1bc78d83788348c3ed205faf5dc0
LemosElias <elilemos786@gmail.com>
2024-10-23 14:03:03 -0300

    se agrega la funcionalidad de combox

--- src/Controller/AddAccountSeatController.java
+++ src/Controller/AddAccountSeatController.java
@@ -1,10 +1,16 @@
 //Clase encargada del comportamiento de la vista de agregar asiento//
 package Controller;
 
+import ConnectionsBD.AccountSeatConnection;
+import Model.Account;
 import Model.User;
 import View.AddAccountSeat;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.DefaultComboBoxModel;
 
 public class AddAccountSeatController implements ActionListener{
     //Atributos//
@@ -13,6 +19,7 @@ public class AddAccountSeatController implements ActionListener{
     private User currentUser=User.getInstancia();
     //Metodos//
     public AddAccountSeatController(){//Conecta el boton Volver con la Clase
+        setCuentasComboBox(); //inicializo el combox//
        this.addAccountSeatView.setTitle("Agregar Asiento"+" - "+currentUser.getUserName()+" ( "+currentUser.getRol().substring(0, 1).toUpperCase()+currentUser.getRol().substring(1).toLowerCase()+ " ) " );
        this.addAccountSeatView.btnEliminar.addActionListener(this);
        this.addAccountSeatView.BtnGuardar.addActionListener(this);
@@ -38,5 +45,33 @@ public class AddAccountSeatController implements ActionListener{
         buttonBack(e);
     }
     
-    
+    //inicializacion para combobox de cuentas//
+    public ArrayList<Account> cuentas () throws IOException,  ClassNotFoundException,   SQLException{
+        AccountSeatConnection conexionCuentas =new AccountSeatConnection();
+        return conexionCuentas.getAccount();
+     }
+   
+    public void setCuentasComboBox() {
+        try {
+            // Obtener la lista de cuentas
+            ArrayList<Account> cuentas = cuentas(); // Asegúrate de que este método esté disponible en el contexto
+
+            // Crear un modelo para el JComboBox
+            DefaultComboBoxModel<String> model = 
+
+                new DefaultComboBoxModel<>();
+
+            // Llenar el modelo con los nombres de las cuentas
+            for (Account cuenta : cuentas) {
+                model.addElement(cuenta.getAccountName()); // Agregar el nombre de la cuenta
+            }
+
+            // Setear el modelo en el JComboBox
+            addAccountSeatView.cBoxAccount.setModel(model); 
+    // Asegúrate de que addSeatView tenga cbbCuentas
+        } catch (IOException | SQLException | ClassNotFoundException e) {
+            e.printStackTrace(); // Manejar la excepción según sea necesario
+        }
+    }
+
 }
--- src/Model/Account.java
+++ src/Model/Account.java
@@ -6,6 +6,7 @@ public class Account {
     private int idAccount;
     private String accountName;
     private int accountCode;
+    private String estado;
     private String accountType;
     private float accountBalance;
     private int receiveBalance;
@@ -21,6 +22,18 @@ public class Account {
         this.receiveBalance=receiveBalance;
     }
 
+    public Account(int idAccount, String accountName, int accountCode, String estado, String accountType, float accountBalance, int receiveBalance) {
+        this.idAccount = idAccount;
+        this.accountName = accountName;
+        this.accountCode = accountCode;
+        this.estado = estado;
+        this.accountType = accountType;
+        this.accountBalance = accountBalance;
+        this.receiveBalance = receiveBalance;
+    }
+    
+    
+
     public Account(String accountName, int accountCode, String accountType, float accountBalance) {
         this.accountName = accountName;
         this.accountCode = accountCode;
@@ -29,7 +42,14 @@ public class Account {
     }
     
     
+    public String getEstado() {
+        return estado;
+    }
+
     //Setters And Getters//
+    public void setEstado(String estado) {    
+        this.estado = estado;
+    }
 
     public String getAccountName() {
         return accountName;
